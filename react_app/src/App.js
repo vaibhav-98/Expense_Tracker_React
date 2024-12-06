e8279e243c79d0d3c55013ed14592243ed5ba249
vaibahv-98 <vaibhavtripathi1512@gmail.com>
2024-12-06 17:48:25 +0530

    add Expense Item logic

--- react_app/src/App.js
+++ react_app/src/App.js
@@ -1,17 +1,21 @@
-import "./App.css"
-import SubComponet from "./Componets/SubComponet"
-
+import "./App.css";
+import ExpenseItem from "./Componets/Expenseitem";
 
 function App() {
-    return (
-    <div className="app-container">
 
-        <h2> Main Componet</h2>
-       <SubComponet/>
-
+     let expenseDate = new Date(2024, 12 , 6);
+     let expenseTitle = "School fee";
+    let expenseAmount = 400
+  return (
+    <div>
+      <h2> Let's get started</h2>
+       <ExpenseItem  
+       date={expenseDate}
+       title={expenseTitle} 
+       amount={expenseAmount}>
+       </ExpenseItem>
     </div>
-    )
-   
+  );
 }
 
-export default App
+export default App;
